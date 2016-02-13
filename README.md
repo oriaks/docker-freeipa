21bcdb7eaddbecd649a59ed7a6914e05afc49dea
Michael Richard <michael.richard@ormuco.com>
2016-02-13 14:29:16 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # docker-freeipa
-Integrated security information management solution combining 389 Directory Server, MIT Kerberos, NTP, DNS, Dogtag (Certificate System)
+
+[FreeIPA](http://www.freeipa.org) is an integrated security information management solution combining Linux (Fedora), 389 Directory Server, MIT Kerberos, NTP, DNS, Dogtag (Certificate System). It consists of a web interface and command-line administration tools.
+
+FreeIPA is an integrated Identity and Authentication solution for Linux/UNIX networked environments. A FreeIPA server provides centralized authentication, authorization and account information by storing data about user, groups, hosts and other objects necessary to manage the security aspects of a network of computers.
+
+FreeIPA is built on top of well known Open Source components and standard protocols with a very strong focus on ease of management and automation of installation and configuration tasks.
---
+++ docker-entrypoint.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+case $1 in
+  "apache2")
+    exec /usr/sbin/apache2ctl -D FOREGROUND
+    ;;
+  "bind9")
+    exec /usr/sbin/named -f -u bind
+    ;;
+  "certmonger")
+    exec /usr/sbin/certmonger -L -S -p /var/run/certmonger.pid -n -d2
+    ;;
+  "dirsrv")
+    exec /usr/sbin/ns-slapd -D /etc/dirsrv/slapd -i /var/run/dirsrv/slapd.pid -w /var/run/dirsrv/slapd.startpid -d0
+    ;;
+  "krb5-admin-server")
+    exec /usr/sbin/kadmind -nofork
+    ;;
+  "krb5-kdc")
+    exec /usr/sbin/krb5kdc -P /var/run/krb5-kdc.pid -n
+    ;;
+  "memcached")
+    exec /usr/bin/memcached -s /var/run/ipa_memcached/ipa_memcached -u www-data -m 64 -c 1024 -P /var/run/ipa_memcached/ipa_memcached.pid
+    ;;
+  "pki-tomcatd")
+    export CATALINA_BASE="/var/lib/pki/pki-tomcat"
+    export CATALINA_PID="/var/run/pki/tomcat/pki-tomcat.pid"
+    export CATALINA_TMPDIR=/var/lib/pki/pki-tomcat/temp
+    export JAVA_OPTS="-DRESTEASY_LIB=/usr/share/java/ -Djava.library.path=/usr/lib/jni"
+    export PKI_PATH="/usr/share/pki/server"
+    export PKI_REGISTRY="/etc/dogtag/tomcat"
+    export PKI_TOTAL_PORTS=7
+    export PKI_TYPE="tomcat"
+    export PKI_VERSION=10.2.6
+    export SECURITY_MANAGER="false"
+    export TOMCAT_LOG="/var/log/pki/pki-tomcat/tomcat-initd.log"
+    export TOMCAT_USER="pkiuser"
+    export TOMCAT7_SECURITY="false"
+    export TOMCAT7_USER="pkiuser"
+    export USE_NUXWDOG="false"
+    export command="start"
+    export pki_instance_id="pki-tomcat"
+    . /usr/share/pki/scripts/operations
+    start
+    sleep 9999999999999999999
+    exit 0
+    ;;
+esac
+
+exec $@
